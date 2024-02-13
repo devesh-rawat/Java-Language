b8b69ad1a52a65e627fd003a14ee7775cb81ee5e
Devesh Rawat <143208051+devesh-rawat@users.noreply.github.com>
2024-02-13 19:41:46 +0530

    files added

---
+++ Chapter1/first.java
@@ -0,0 +1,6 @@
+public class first{
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("This is My first Program in Java Lanuage");
+    }
+}
