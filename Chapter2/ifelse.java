4e01fac5606a6f172284f03f803da8f8ab9afe11
Devesh Rawat <143208051+devesh-rawat@users.noreply.github.com>
2024-02-14 22:54:05 +0530

    done

--- Chapter2/ifelse.java
+++ Chapter2/ifelse.java
@@ -1,5 +1,5 @@
-import java.util.*;
-public class Chapter2{
+
+public class ifelse{
     public static void main(String[] args) {
         System.out.println("This is a ifelse program in java");
         System.out.println("Enter Your Age:");
